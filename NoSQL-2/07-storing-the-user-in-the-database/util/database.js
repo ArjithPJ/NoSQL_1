46f63d1dccbde3901c31bce0890e467fe79ec3a8
Arjith P J <pjarjith@gmail.com>
2024-05-07 19:36:42 +0530

    Users added

--- NoSQL-2/07-storing-the-user-in-the-database/util/database.js
+++ NoSQL-2/07-storing-the-user-in-the-database/util/database.js
@@ -5,7 +5,7 @@ let _db;
 
 const mongoConnect = callback => {
   MongoClient.connect(
-    'mongodb://localhost:27017/'
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
   )
     .then(client => {
       console.log('Connected!');
