57b109ebd285c01160949f1d417a0c6694072101
Arjith P J <pjarjith@gmail.com>
2024-05-09 16:10:07 +0530

    App completed

--- controllers/shop.js
+++ controllers/shop.js
@@ -95,8 +95,9 @@ exports.postOrder = (req, res, next) => {
   .populate('cart.items.productId')
   .execPopulate()
   .then(user => {
+    console.log(user.cart.items);
     const products = user.cart.items.map(i => {
-      return {quantity: i.quantity, product: i.productId}; 
+      return {quantity: i.quantity, product: { ...i.productId._doc}}; 
     });
     const order =new Order({
       user: {
@@ -108,20 +109,23 @@ exports.postOrder = (req, res, next) => {
     return order.save();
   })
   .then(result => {
+    req.user.clearCart();
+    
+  })
+  .then(() => {
     res.redirect('/orders');
   })
   .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
-    .then(orders => {
-      res.render('shop/orders', {
-        path: '/orders',
-        pageTitle: 'Your Orders',
-        orders: orders
-      });
-    })
-    .catch(err => console.log(err));
+  Order.find({"user.userId": req.user._id})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log(err));
 };
